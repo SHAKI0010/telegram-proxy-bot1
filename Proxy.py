56db71b6ab1b118a89ed6721144c5dc628b85d65
SHAKI <arman1387free@gmail.com>
2025-11-25 14:14:53 +0330

    Update Proxy.py

--- Proxy.py
+++ Proxy.py
@@ -352,6 +352,14 @@ def main():
     except Exception as e:
         logger.exception(f"Polling error: {e}")
         time.sleep(2)
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Render!"
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
